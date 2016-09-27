568ac9f4b8e1a7f7463d170b459554a4a3dd3965
esotericabsurdist <esotericabsurdist@gmail.com>
2016-09-26 23:57:09 -0700

    formatted and validated files

--- LearningWebAppDev/Chapter4/Example2/app.js
+++ LearningWebAppDev/Chapter4/Example2/app.js
@@ -3,34 +3,21 @@ var main = function () {
 
     var addCommentFromInputBox = function () {
         var $new_comment;
-
         if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment = $("<p class=\"user0\">").text($(".comment-input input").val());
             $new_comment.hide();
             $(".comments").append($new_comment);
             $new_comment.fadeIn();
             $(".comment-input input").val("");
         }
     };
-    var addCommentFromInputBox1 = function () {
-        /*
-        var $new_comment;
-
-        if ($(".comment-input1 input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input1 input").val());
-            $new_comment.hide();
-            $(".comments1").append($new_comment); // append to .comments, instead of .comments 1
-            $new_comment.fadeIn();
-            $(".comment-input1 input").val("");
-        }
-		*/
-		
+    var addCommentFromInputBox1 = function () {	
 		var $new_comment1;
 
         if ($(".comment-input1 input").val() !== "") {
-            $new_comment1 = $("<p>").text($(".comment-input1 input").val());
+            $new_comment1 = $("<p class=\"user1\">").text($(".comment-input1 input").val());
             $new_comment1.hide();
-            $(".comments1").append($new_comment1);
+            $(".comments").append($new_comment1);
             $new_comment1.fadeIn();
             $(".comment-input1 input").val("");
         }
